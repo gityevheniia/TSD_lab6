4cd3040de91c75a9e3d43c675f3e1d1b991dfb8d
gityevheniia <bukhalo.yevheniia@lll.kpi.ua>
2024-12-16 21:52:57 +0200

    Додавання UserManager з можливістю додавання користувачів

---
+++ UserManager.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+
+namespace AccessControlSystem
+{
+    public class UserManager
+    {
+        private List<User> users = new List<User>();
+
+        public void AddUser(int id, string name, string role)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Помилка: Ім'я не може бути порожнім!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(role))
+            {
+                Console.WriteLine("Помилка: Роль не може бути порожньою!");
+                return;
+            }
+
+            users.Add(new User(id, name, role));
+            Console.WriteLine("Користувача успішно додано!");
+        }
+
+        public void ListUsers()
+        {
+            Console.WriteLine("Список користувачів:");
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
