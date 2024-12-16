e396b127047f5a8897f7ade2c8588900aba32b9b
gityevheniia <bukhalo.yevheniia@lll.kpi.ua>
2024-12-16 21:52:02 +0200

    Ініціалізація проекту та базовий клас User

---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public User(int id, string name, string role)
+        {
+            Id = id;
+            Name = name;
+            Role = role;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Role: {Role}";
+        }
+    }
+}
