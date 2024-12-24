0714d33588c2abb5150240fb0e72d25c105f2681
gityevheniia <bukhalo.yevheniia@lll.kpi.ua>
2024-12-24 19:20:32 +0200

    Додано у Entities класи Pass та AccesRequest

---
+++ AccessControlSystem/Entities/AccessRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class AccessRequest
+    {
+        public int Id { get; set; }
+        public int PassId { get; set; }
+        public string RequestedZone { get; set; }
+        public DateTime RequestDate { get; set; }
+        public bool IsApproved { get; set; }
+        public string DenialReason { get; set; }
+    }
+}
---
+++ AccessControlSystem/Entities/Pass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Pass
+    {
+        public int Id { get; set; }
+        public string PassNumber { get; set; }
+        public string EmployeeName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
